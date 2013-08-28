08b8e7651e9064bf25fee782c80946666d4317f1
Branko Ajzele <ajzele@gmail.com>
2013-08-28 16:10:20 +0200

    Initial commit, working Foggyline_Stripe extension.

---
+++ app/code/community/Foggyline/Stripe/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class Foggyline_Stripe_Helper_Data extends Mage_Payment_Helper_Data
+{
+}
---
+++ app/code/community/Foggyline/Stripe/Model/Source/Cctype.php
@@ -0,0 +1,6 @@
+<?php
+
+class Foggyline_Stripe_Model_Source_Cctype extends Mage_Payment_Model_Source_Cctype
+{
+    protected $_allowedTypes = array('AE','VI','MC','DI','JCB','OT');
+}
